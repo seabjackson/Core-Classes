c9f4a5380753220c5b4eb06929198aff08d63774
Seab <honeyboyseab@gmail.com>
2020-09-23 03:00:38 -0400

    Created a Network Controller which launches URLSessionDataTask and obtains the data if no error occured

---
+++ Core Classes/Network Layer/NetworkController.swift
@@ -0,0 +1,25 @@
+//
+//  NetworkControllerProtocol.swift
+//  Core Classes
+//
+//  Created by Seab Jackson on 9/23/20.
+//  Copyright © 2020 Seab Jackson. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+protocol NetworkControllerProtocol: class {
+    func get<T>(type: T.Type, from url: URL) -> AnyPublisher<T, Error> where T: Codable
+}
+
+
+final class NetworkController: NetworkControllerProtocol {
+    func get<T: Codable>(type: T.Type, from url: URL) -> AnyPublisher<T, Error> {
+        let urlRequest = URLRequest(url: url)
+        return URLSession.shared.dataTaskPublisher(for: urlRequest)
+            .map(\.data)
+            .decode(type: T.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
