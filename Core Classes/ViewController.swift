29e10138315d1301ad3694b07568e131204ec94e
Seab <honeyboyseab@gmail.com>
2020-09-23 02:10:51 -0400

    Initial Commit

---
+++ Core Classes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Core Classes
+//
+//  Created by Seab Jackson on 9/23/20.
+//  Copyright © 2020 Seab Jackson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
