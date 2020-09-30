363b6a9dd7e3e14940006449208872aed022add6
Seab <honeyboyseab@gmail.com>
2020-09-29 20:41:42 -0400

    Provided proper constraints, and fonts for all labels in the 'CoreClassesCell' and moved helper code into a Single file Utils

--- Core Classes/Views/CoreClassesCell.swift
+++ Core Classes/Views/CoreClassesCell.swift
@@ -35,9 +35,8 @@ class CoreClassesCell: UITableViewCell {
         categoryLabel.text  = coreClass.modality ?? ""
         setColorFor(modality: coreClass.modalityColor)
         classTitleLabel.text = coreClass.title ?? ""
-        
-        //        instructorLabel.text = coreClass.instructor ?? ""
-        //        descriptionLabel.text = coreClass.description ?? ""
+        instructorLabel.text = coreClass.instructor ?? ""
+        descriptionLabel.text = coreClass.description ?? "No description yet for this activity" // assumption on if no description available
        
       
     }
@@ -57,58 +56,3 @@ class CoreClassesCell: UITableViewCell {
     
 }
 
-extension UIColor {
-    
-    convenience init(hex: String, alpha: CGFloat = 1.0) {
-        var hexFormatted: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
-        
-        if hexFormatted.hasPrefix("#") {
-            hexFormatted = String(hexFormatted.dropFirst())
-        }
-        
-        assert(hexFormatted.count == 6, "Invalid hex code used.")
-        
-        var rgbValue: UInt64 = 0
-        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
-        
-        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-                  alpha: alpha)
-    }
-    
-}
-
-enum ModalityColors: String {
-    case weights = "Weights"
-    case hiit = "HIIT"
-    case dance = "Dance"
-    case yoga = "Yoga"
-}
-
-
-struct Constants {
-    static let WEIGHTCOLOR = "#49A3AA"
-    static let HIITCOLOR = "#AB3D6E"
-    static let DANCECOLOR = "#F09466"
-    static let YOGACOLOR = "#1B488C"
-    
-    struct Theme {
-        static let mainColor = "#fef9ec"
-    }
-    
-    struct Titles {
-        static let HomeScreenTitle = "Classes"
-    }
-    
-    struct Times {
-        static let SIXTYMINUTES = 60
-    }
-    
-    struct URL {
-        static let scheme = "https"
-        static let host = "core-class-search.herokuapp.com"
-        static let path = "/classes"
-        static let invalidURL = "The URL has Invalid Components: "
-    }
-}
